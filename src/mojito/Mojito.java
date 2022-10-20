4c4c681898dee67c16d5327f1a4c643966a38634
Aula L 5-2 <l52@academico.unab.ad>
2022-10-20 11:20:17 -0500

    rose

--- src/mojito/Mojito.java
+++ src/mojito/Mojito.java
@@ -4,7 +4,12 @@ package mojito;
 public class Mojito {
 
     public static void main(String[] args) {
-        
+        Rosas rose = new Rosas();
+        rose.setFamilia("Rosaceae");
+        rose.setDivision("Magnoliophyta");
+        rose.setOrden("Rosales");
+        rose.setGenero("Rosa");
+        System.out.println("Las rosas pertenecen a la familia "+rose.getFamilia()+" de la división "+rose.getDivision()+" de orden "+rose.getOrden()+" del género "+rose.getGenero()+".");
     }
     
 }
--- src/mojito/Rosas.java
+++ src/mojito/Rosas.java
@@ -1,66 +1,44 @@
-
 package mojito;
 
 public class Rosas {
-  private String familia;
-  private String division;
-  private String orden;
-  private String genero;
 
-    /**
-     * @return the familia
-     */
+    private String familia;
+    private String division;
+    private String orden;
+    private String genero;
+        
     public String getFamilia() {
         return familia;
     }
 
-    /**
-     * @param familia the familia to set
-     */
     public void setFamilia(String familia) {
         this.familia = familia;
     }
 
-    /**
-     * @return the division
-     */
     public String getDivision() {
         return division;
     }
 
-    /**
-     * @param division the division to set
-     */
     public void setDivision(String division) {
         this.division = division;
     }
 
-    /**
-     * @return the orden
-     */
     public String getOrden() {
         return orden;
     }
 
-    /**
-     * @param orden the orden to set
-     */
     public void setOrden(String orden) {
         this.orden = orden;
     }
 
-    /**
-     * @return the genero
-     */
     public String getGenero() {
         return genero;
     }
 
-    /**
-     * @param genero the genero to set
-     */
     public void setGenero(String genero) {
         this.genero = genero;
     }
-  
+    public void germinar(){System.out.println("Puedes sembrar en primavera o en otoño, en semillero o directamente en la tierra.");}
+    public void crecer(){System.out.println("Las rosas crecen todo el año hasta principios de invierno.");}
+    public void florecer(){System.out.println("Las rosas florecen en la primavera.");}
 }
